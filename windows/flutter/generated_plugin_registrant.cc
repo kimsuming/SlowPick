bcf62e5a896148c7063d3b59df2855cab9939e35
kimsuming <marianne.gg53@gmail.com>
2026-05-11 23:04:37 +0900

    유저 기능을 위한 Amplify 설치

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <amplify_db_common/amplify_db_common_plugin.h>
 #include <cloud_firestore/cloud_firestore_plugin_c_api.h>
 #include <firebase_core/firebase_core_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AmplifyDbCommonPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AmplifyDbCommonPlugin"));
   CloudFirestorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("CloudFirestorePluginCApi"));
   FirebaseCorePluginCApiRegisterWithRegistrar(
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  amplify_db_common
   cloud_firestore
   firebase_core
 )
